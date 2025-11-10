424d7b0bedb698531a8749715a8f915ad9a706bc
Daniel Paez <dfpaez@unimagdalena.edu.co>
2025-11-10 18:57:37 -0500

    Debug and improvement of Normal Functions

--- funciones.js
+++ funciones.js
@@ -1,13 +1,23 @@
 //Funciones normales
 
-const a = 7
-const b= 7
+const a = 5
+const b= 8
+const rnd = Math.floor(10*Math.random())
 function sum(a,b){
     return a+b
 }
 
-function resta(sum,b){
-    return sum-b
+function resta(a,b){
+    return a-b
 }
+
+function sqrt (n,pw){
+    return n**pw
+}
+
+console.log(sqrt(sum(resta(b,a),rnd),2)) //Resta b-a, le suma un random de 1-9 y lo eleva a la 2
+
+//funciones puras
+
 
 
